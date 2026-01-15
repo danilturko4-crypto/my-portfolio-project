06e8410e0bbea71d39ead205c400b77b4ae8ed57
Turko Danil <danilturko4@gmail.com>
2026-01-16 01:52:09 +0500

    Create doctor booking interface

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import store from './store'
+import './style.css'
 
-createApp(App)
-  .use(store)
-  .mount('#app')
+createApp(App).mount('#app')
